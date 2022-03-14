5b298785b6922ad96024fc6b71f08c5f8eaf1553
pbulthuis <61009960+pbulthuis@users.noreply.github.com>
2022-03-14 10:26:29 +0100

    Update natuurkunde.py

--- natuurkunde.py
+++ natuurkunde.py
@@ -7,8 +7,27 @@ dx = 0
 v = 0
 c = 0
 gc = 0.0000000000667384                 # Nm²kg⁻²
-ma = 5972000000000000000000000          # kg
-ra = 6371000                            # m
+mearth = 5972000000000000000000000          # mass of earth - kg
+rearth = 6371000                            # radius of earth - m
+mmoon = 73500000000000000000000            # mass of moon - kg 
+rmoon = 1738000                            # radius of moon - m
+mmercury = 330000000000000000000000  # mass of mercury - kg
+rmercury = 2440000                   # radius of mercury - m
+
+print("Choose: 1 Earth | 2 Moon | 3 Mercurius") 
+chp = int(input())
+if chp == 1:
+    mp = mearth
+    rp = rearth
+elif chp == 2:
+    mp = mmoon
+    rp = rmoon
+elif chp == 3:
+    mp = mmercury
+    rp = rmercury
+else:
+    exit()
+
 x = int(input("Input height (meters):"))
 m = int(input("Input weight (kg):"))
 o = int(input("Input the frontal area (m²):"))
@@ -32,9 +51,20 @@ pa = []
 
 while(x > 0):
     print(x)
-    fg = gc * ((ma * m)/((ra + x)**2))      #gravitational force
-    r = 1.293 * (0.5**(x / 5500))           #rho, air density
-    flw = 0.5 * r * cw * o * v * v          #air resistance
+
+    if x < 10000000:
+        dt = 0.01
+    else:
+        dt = 15
+
+    fg = gc * ((mp * m)/((rp + x)**2))      #gravitational force
+    
+    if chp == 1:
+        r = 1.293 * (0.5**(x / 5500))           #rho, air density
+        flw = 0.5 * r * cw * o * v * v          #air resistance
+    else:
+        flw = 0
+    
     fres = fg - flw
     a = fres / m 
     dv = a * dt
@@ -66,21 +96,19 @@ plt.ylabel('Velocity (m/s)')
 plt.title('Velocity')
 
 plot3 = plt.figure(3)
-plt.plot(pt, pflw)
-plt.xlabel('Time (s)')
-plt.ylabel('Air resistance (N)')
-plt.title('Air resistance')
-
-plot4 = plt.figure(4)
 plt.plot(pt, pa)
 plt.xlabel('Time (s)')
 plt.ylabel('Acceleration (m/s²)')
 plt.title('Acceleration')
 
+if chp == 1:
+    plot4 = plt.figure(4)
+    plt.plot(pt, pflw)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Air resistance (N)')
+    plt.title('Air resistance')
+
 plt.show()
-
-
-
 
 
 
